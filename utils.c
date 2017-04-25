5b88e0e9e6b84fcdfc0a0a77b26c8d0d5d33edc3
KerwinKoo <gukaiqiang@gmail.com>
2017-04-25 20:45:37 +0800

    add my curl utis

--- utils.c
+++ utils.c
@@ -12,8 +12,12 @@
 #include <ctype.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
+#include <sys/stat.h>
 #include <dirent.h>
 #include <errno.h>
+#include <curl/curl.h>
+#include <curl/easy.h>
+
 #include "utils.h"
 
 #define PROC_PATH "/proc"
@@ -454,4 +458,295 @@ get_pure_md5sum(const char *md5_sum, char *pure_md5_buf, int buf_len) {
 	}
 
 	return 0;
+}
+
+/*
+ * NAME curl_init()
+ *
+ * DESCRIPTION
+ *
+ * curl_init() will call libcurl func curl_global_init(), which should be invoked 
+ * exactly once for each application that uses libcurl and before any call of 
+ * other libcurl functions.
+ *
+ * This function is not thread-safe! So I allways dont't use it ---- By KerwinKoo
+ */
+void curl_init() {
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+}
+
+static int dl_progress(void *clientp,double dltotal,double dlnow,double ultotal,double ulnow) {
+    // if (dlnow && dltotal)
+    //     printf("dl:%3.0f%%\r",100*dlnow/dltotal); //shenzi prog-mon 
+	// //	printf("dl:%3.0f\r",100*dlnow/dltotal); //shenzi prog-mon 
+    // fflush(stdout);
+    return 0;
+}
+
+// dl_write callback func, write bytes into File 
+static size_t dl_write(void *buffer, const size_t size, const size_t nmemb, void *stream) {    
+	return fwrite(buffer, size, nmemb, (FILE*)stream); 
+}
+
+
+static size_t write_to_mycurl_string(void *buffer, 
+									const size_t size, 
+									const size_t nmemb, 
+									struct mycurl_string *s) {
+	size_t new_len = s->len + size*nmemb;
+	// s->ptr = realloc(s->ptr, new_len + 1); // realloc is NOT recommended
+	size_t malloc_len = new_len + 1;
+	char *tmp_p = os_malloc(malloc_len);
+	if (tmp_p == NULL) {
+		return 0;
+	}
+
+	memset(tmp_p, 0, malloc_len);
+	memcpy(tmp_p, s->ptr, s->len);
+	free(s->ptr);
+	s->ptr = tmp_p;
+	memcpy(s->ptr + s->len, buffer, size*nmemb);
+
+	return size*nmemb;
+}
+
+struct mycurl_string *mycurl_string_init(struct mycurl_string *stream) {
+	stream->len = 0;
+	stream->ptr = os_malloc(stream->len+1);
+
+	if (stream->ptr == NULL) {
+		return NULL;
+	}
+
+	stream->ptr[0] = '\0';
+	return stream;
+}
+
+void mycurl_string_free(struct mycurl_string *stream) {
+	if (stream != NULL && stream->ptr != NULL) {
+		free(stream->ptr);
+	}
+}
+
+// curl_download main curl download func
+// return : 0=succeed
+// 			1-failed/err with some reason that could not download, reason code 
+//				saved in state_code argument.
+int download(char *url, 
+			char *target_filename, 
+			int resume_enable,
+			long timeout, 
+			int *state_code,
+			double *download_size) {
+				
+	if (target_filename == NULL) {
+		return 1;
+	}
+
+	CURL *curl;
+    FILE *fp;
+	CURLcode curl_retval;
+	long http_response;
+	double dl_size;
+	int could_be_resume = 0;
+	struct stat st={0};
+	int file_stat = 0;
+	int ret = 1;
+
+	/* dl_lowspeed_time seconds while below low spped limit before aborting 
+	 * dl_lowspeed_bytes is the limitation. 
+	 */
+    long dl_lowspeed_bytes = 1000; //1K
+	*state_code = CURL_OK;
+    long dl_lowspeed_time = 60; //sec
+	if (timeout <= 0) {
+		*state_code = CURL_TIMEOUT_SET_ERR;
+		return ret;
+	}
+	file_stat = stat(target_filename, &st);
+    if (file_stat) {
+		// file is not existed, so create new one
+		;
+    }else{
+		//file is existed, so it could be using resume func
+		if (resume_enable) {
+			could_be_resume = 1;
+		}else{
+			if( remove(target_filename) ) { // remove return 0 if succeed
+				*state_code = CURL_FILE_DEL_ERR;
+				return ret;
+			}
+		}
+	}
+
+	if(!(fp=fopen(target_filename, "ab")/*append binary*/)) {
+		*state_code = CURL_FILE_OPEN_ERR;
+		return ret; 
+	}
+	curl = curl_easy_init();
+
+	if (curl) {
+        //http://linux.die.net/man/3/curl_easy_setopt
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        /*callbacks*/
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, dl_write);
+        curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, dl_progress);
+        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
+
+        /*curl will keep running -so you have the freedom to recover 
+        from network disconnects etc in your own way without
+        distrubing the curl task in hand. ** this is by design :p ** */ 
+		// everything need be downloaded in CURLOPT_TIMEOUT limit
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT, timeout);
+        curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 30);
+        /*set up min download speed threshold & time endured before aborting*/
+        curl_easy_setopt(curl, CURLOPT_LOW_SPEED_LIMIT, dl_lowspeed_bytes); //bytes/sec
+
+		/* seconds while below low spped limit before aborting */
+        curl_easy_setopt(curl, CURLOPT_LOW_SPEED_TIME, dl_lowspeed_time); 
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+
+		if (resume_enable && could_be_resume) {
+			curl_easy_setopt(curl, CURLOPT_RESUME_FROM,st.st_size);
+		}
+
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1); // handle 302 and 301
+        /*digitals to descrip info,
+		 *uncomment this to get curl to tell you what its up to*/
+#if CURL_DEBUG
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+#endif
+        if(CURLE_OK != (curl_retval = curl_easy_perform(curl))) {
+			switch(curl_retval) {
+				//Transferred a partial file
+				//all defined in curl/curl.h 
+				default: //suggest quitting on unhandled error
+					*state_code = curl_retval;
+			};
+
+            curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &dl_size);
+			*download_size = dl_size;
+            curl_retval=curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_response);
+
+            //see: http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+            switch(http_response){
+				//eg connection down  from kick-off ~suggest retrying till some max limit
+				case 200: //yay we at least got to our url
+					*state_code = CURL_HTTP_200;
+					break;
+				case 404:
+					ret = 1;
+					break;
+				case 206:
+				case 416: //http://www.checkupdown.com/status/E416.html
+				default: //suggest quitting on an unhandled error
+					*state_code = CURL_HTTP_OTHER;
+					break;
+            };
+		} else { //our work here is done ;)
+            ret = 0;
+        }
+		fclose(fp);
+
+        if (curl){
+			curl_easy_cleanup(curl);
+		}
+	}
+
+	return ret;
+}
+
+// net_visit
+// return : 0=succeed
+// 			1-failed/err with some reason that could not download, reason code 
+//				saved in state_code argument.
+int 
+net_visit(char *url, 
+			struct mycurl_string *s,
+			int method,
+			char *post_buf,
+			long timeout, 
+			int *state_code,
+			double *down_size) {
+
+	CURL *curl;
+	CURLcode curl_retval;
+	long http_response;
+	double dl_size;
+	int could_be_resume = 0;
+	struct stat st={0};
+	int file_stat = 0;
+	int ret = 1;
+
+	/* dl_lowspeed_time seconds while below low spped limit before aborting 
+	 * dl_lowspeed_bytes is the limitation. 
+	 */
+    long dl_lowspeed_bytes = 1000; //1K
+	*state_code = CURL_OK;
+    long dl_lowspeed_time = 60; //sec
+	if (timeout <= 0) {
+		*state_code = CURL_TIMEOUT_SET_ERR;
+		return ret;
+	}
+
+	curl = curl_easy_init();
+	if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        /*callbacks*/
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_mycurl_string);
+        curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, dl_progress);
+        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT, timeout);
+        curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_easy_setopt(curl, CURLOPT_LOW_SPEED_LIMIT, dl_lowspeed_bytes); //bytes/sec
+
+		/* seconds while below low spped limit before aborting */
+        curl_easy_setopt(curl, CURLOPT_LOW_SPEED_TIME, dl_lowspeed_time); 
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, s);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1); // handle 302 and 301
+
+		if (method == POST) {
+			char *self_post_buf = post_buf == NULL ? "/0":post_buf;
+			curl_easy_setopt(curl, CURLOPT_POSTFIELDS, self_post_buf);
+		}
+        /*digitals to descrip info,
+		 *uncomment this to get curl to tell you what its up to*/
+#if CURL_DEBUG
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+#endif
+        if(CURLE_OK != (curl_retval = curl_easy_perform(curl))) {
+			switch(curl_retval) {
+				//Transferred a partial file
+				//all defined in curl/curl.h 
+				default: //suggest quitting on unhandled error
+					*state_code = curl_retval;
+			};
+
+            curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &dl_size);
+			*down_size = dl_size;
+            curl_retval=curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_response);
+
+            switch(http_response){
+				case 200: //yay we at least got to our url
+					*state_code = CURL_HTTP_200;
+					break;
+				case 404:
+					ret = 1;
+					break;
+				case 206:
+				case 416: //http://www.checkupdown.com/status/E416.html
+				default: //suggest quitting on an unhandled error
+					*state_code = CURL_HTTP_OTHER;
+					break;
+            };
+		} else { //our work here is done ;)
+            ret = 0;
+        }
+
+        if (curl){
+			curl_easy_cleanup(curl);
+		}
+	}
+
+	return ret;
 }
--- utils.h
+++ utils.h
@@ -1,10 +1,11 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-#define _GNU_SOURCE
-
 #include <sys/types.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <curl/curl.h>
 
 // #define DEBUG 0
 
@@ -14,8 +15,33 @@
 #define DEBUG_TEST 0
 #endif
 
+#ifndef os_malloc
+#define os_malloc(s) malloc((s))
+#endif
+#ifndef os_memset
+#define os_memset(s, c, n) memset(s, c, n)
+#endif
+#ifndef os_free
+#define os_free(p) free((p))
+#endif
 
 #define         STRCMP(a, R, b)         (strcmp(a, b) R 0)
+
+/* curl define */
+// curl methods 
+#define GET 0
+#define POST 1
+
+#define CURL_DEBUG 0
+
+#define CURL_OK 0x900
+#define CURL_TIMEOUT_SET_ERR 0x901
+#define CURL_FILE_DEL_ERR 0x902
+#define CURL_FILE_OPEN_ERR 0x903
+#define CURL_PERFORM_UNHANDLED_ERR 0x904
+#define CURL_HTTP_200 0x905
+#define CURL_HTTP_404 0x906
+#define CURL_HTTP_OTHER 0x999
 
 #define debug_print(fmt, ...) \
             do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
@@ -23,6 +49,13 @@
 
 
 #define debug_kprint(...) \ do { if (DEBUG) \ printk("DRIVER_NAME:"); \ printk(__VA_ARGS__); \ 			printk("\n"); \ } while (0)
+
+
+/* structs define */
+struct mycurl_string {
+	char *ptr;
+	size_t len;
+};
 
 int IsPathExist(const char *path_name);
 int SafeWrite2File(const char *fn, const char *buf, size_t buflen);
@@ -38,4 +71,23 @@ int http_url_format(const char *orig_url, char *url_result_buf, int buf_len);
 int get_child_pids(int parent_pid, char *exec_cmd, int *child_pids, int len, char stat);
 int get_ancestor_pid(char *exec_cmd, int target_pid);
 int get_pure_md5sum(const char *md5_sum, char *pure_md5_buf, int buf_len);
+
+void curl_init();
+void mycurl_string_free(struct mycurl_string *stream);
+struct mycurl_string *mycurl_string_init(struct mycurl_string *stream);
+int download(char *url, 
+			char *target_filename, 
+			int resume_enable,
+			long timeout, 
+			int *state_code,
+			double *download_size);
+
+int net_visit(char *url, 
+			struct mycurl_string *s,
+			int method,
+			char *post_buf,
+			long timeout, 
+			int *state_code,
+			double *down_size);
+
 #endif // UTIL_H
